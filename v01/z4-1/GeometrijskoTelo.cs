6531e8c04a455dc3a77047494fa24c96809e16b4
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-13 14:52:25 +0100

    Create GeometrijskoTelo.cs

---
+++ v01/z4-1/GeometrijskoTelo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z11
+{
+    public abstract class GeometrijskoTelo
+    {
+        public abstract double GetP();
+        public abstract double GetV();
+    }
+}
