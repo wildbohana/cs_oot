01a29852000bb2b4a4fbc42ca03fccbdd4279389
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-13 14:52:53 +0100

    Create Kocka.cs

---
+++ v01/z4-1/Kocka.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z11
+{
+    public class Kocka : Kvadar
+    {
+
+        public Kocka(double a) : base(a, a, a)
+        {
+			// zašto je prazno?
+			// da li stvarno ništa ne menjamo iz Kvadra od metoda?
+			
+			// base pristupa natklasi valjda, tj pristupa Kvadru ??
+        }
+    }
+}
