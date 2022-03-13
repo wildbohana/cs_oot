45f3c88f2e111f6d0894c4308fe52a27a725f5b4
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-13 14:53:47 +0100

    Create GeometrijskaFigura.cs

---
+++ v01/z5/GeometrijskaFigura.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z3
+{
+   public interface IGeometrijskaFigura		// interfejs
+    {
+        double GetO();
+        double GetP();
+    }
+}
