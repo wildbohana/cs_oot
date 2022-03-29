7d9ae494a7ae6e46991251937a02b69d9ab94cd8
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 23:29:25 +0200

    Update Program.cs

--- v04/EvidencijaZaposlenih/Program.cs
+++ v04/EvidencijaZaposlenih/Program.cs
@@ -10,19 +10,19 @@ namespace EvidencijaZaposlenih
     {
         static void Main(string[] args)
         {
-			Zaposleni z = new Zaposleni(1, "Marko", "Markovic", 2500, 5, 0);
-			Console.WriteLine(z);
-			Console.WriteLine();
+		Zaposleni z = new Zaposleni(1, "Marko", "Markovic", 2500, 5, 0);
+		Console.WriteLine(z);
+		Console.WriteLine();
 
-			EvidencijaZaposlenih e = new EvidencijaZaposlenih("FTN", "Trg Dositeja Obradovica 6");
+		EvidencijaZaposlenih e = new EvidencijaZaposlenih("FTN", "Trg Dositeja Obradovica 6");
 
-			Console.WriteLine("Ispisujem evidenciju zaposlenih: \n\n");
-			e.UcitajZaposlene("Zaposleni.txt");
-			Console.WriteLine(e);
+		Console.WriteLine("Ispisujem evidenciju zaposlenih: \n\n");
+		e.UcitajZaposlene("Zaposleni.txt");
+		Console.WriteLine(e);
 
-			Console.WriteLine("Najbolji radnik tj. radnik sa najvecom platom: \n" + e.NajboljiRadnik());
+		Console.WriteLine("Najbolji radnik tj. radnik sa najvecom platom: \n" + e.NajboljiRadnik());
 
-			Console.ReadLine();
-		}
+		Console.ReadLine();
+	}
     }
 }
