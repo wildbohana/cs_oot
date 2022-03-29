d65899b37b3d640c015510dc8c038475268ecfcb
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:38:15 +0200

    Create Stavka.cs

---
+++ v03/5/Stavka.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z5
+{
+    class Stavka
+    {
+		// polja
+        private string naziv;
+        private double cena;
+
+		// properties
+        public string Naziv
+        {
+            get { return naziv; }
+            set { naziv = value; }
+        }
+
+        public double Cena
+        {
+            get { return cena; }
+            set { cena = value; }
+        }
+
+		// konstruktor
+        public Stavka(string naziv, double cena)
+        {
+            Naziv = naziv;
+            Cena = cena;
+        }
+
+		// ispis
+        public override string ToString()
+        {
+            return Naziv + " " + Cena;
+        }
+
+    }
+}
