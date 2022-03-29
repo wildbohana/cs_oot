da49d47a420986d489f09b7be2e2d5ab40696f1d
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 23:27:51 +0200

    Create Izvestaj.cs

---
+++ v04/EvidencijaZaposlenih/Izvestaj.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencijaZaposlenih
+{
+	// interfejs - ne implementiraš ništa ovde
+    interface Izvestaj
+    {
+        int Plata(int id);
+    }
+}
