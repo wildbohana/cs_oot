96bc85774145c714ee65b9801902cec177bd61e7
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 23:29:02 +0200

    Update Izvestaj.cs

--- v04/EvidencijaZaposlenih/Izvestaj.cs
+++ v04/EvidencijaZaposlenih/Izvestaj.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EvidencijaZaposlenih
 {
-	// interfejs - ne implementiraš ništa ovde
+    // interfejs - ne implementiraš ništa ovde
     interface Izvestaj
     {
         int Plata(int id);
