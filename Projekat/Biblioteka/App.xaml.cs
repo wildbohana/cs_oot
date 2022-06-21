132f219d59440a6b4a10c14e0520bc0c3380fd4f
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-06-21 21:37:12 +0200

    Add files via upload

---
+++ Projekat/Biblioteka/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Biblioteka.Knjige;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Biblioteka
+{
+    public partial class App : Application
+    {
+        #region AUTO INKREMENTI ZA ID
+        private static int idCounter = 1;			// Za biblioteke
+        private static int idKorisnikaCnt = 1;		// Za korisnika
+        private static int idKnjigeCnt = 1;         // Za knjigu
+        #endregion
+
+        #region PRIVREMENE REFERENCE NA POJEDINE (SELEKTOVANE) OBJEKTE
+        static Biblioteke.Biblioteka selektovanaBiblioteka = null;
+        static Knjige.Knjiga selektovanaKnjiga = null;
+        static Korisnici.Korisnik selektovaniKorisnik = null;
+        #endregion
+
+        #region KOLEKCIJE ZA SVE BIBLIOTEKE, KNJIGE I KORISNIKE
+        static ObservableCollection<Biblioteke.Biblioteka> biblioteke = new ObservableCollection<Biblioteke.Biblioteka>();
+        static ObservableCollection<Knjige.Knjiga> sveKnjige = new ObservableCollection<Knjige.Knjiga>();
+        static ObservableCollection<Korisnici.Korisnik> sviKorisnici = new ObservableCollection<Korisnici.Korisnik>();
+        private static ObservableCollection<Knjiga> referencaNaKnjigePoBiblioteci = null;
+        #endregion
+
+        #region PROPERTY KLASE APP (KOLEKCIJE, ID COUNTERS, ...)
+        internal static ObservableCollection<Knjige.Knjiga> ReferencaNaKnjigePoBiblioteci { get => referencaNaKnjigePoBiblioteci; set => referencaNaKnjigePoBiblioteci = value; }
+        internal static ObservableCollection<Biblioteke.Biblioteka> Biblioteke { get => biblioteke; set => biblioteke = value; }
+        internal static ObservableCollection<Knjige.Knjiga> SveKnjige { get => sveKnjige; set => sveKnjige = value; }
+        internal static ObservableCollection<Korisnici.Korisnik> SviKorisnici { get => sviKorisnici; set => sviKorisnici = value; }
+        internal static Biblioteke.Biblioteka SelektovanaBiblioteka { get => selektovanaBiblioteka; set => selektovanaBiblioteka = value; }
+        internal static Knjige.Knjiga SelektovanaKnjiga { get => selektovanaKnjiga; set => selektovanaKnjiga = value; }
+        internal static Korisnici.Korisnik SelektovaniKorisnik { get => selektovaniKorisnik; set => selektovaniKorisnik = value; }
+        public static int IdCounter { get => idCounter; set => idCounter = value; }
+        public static int IdKorisnikaCnt { get => idKorisnikaCnt; set => idKorisnikaCnt = value; }
+        public static int IdKnjigeCnt { get => idKnjigeCnt; set => idKnjigeCnt = value; }
+        #endregion
+    }
+}
---
+++ Projekat/Biblioteka/GlavniProzor.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows;
+
+namespace Biblioteka
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Program.PilotData pd = new Program.PilotData();
+            pd.Pivot_Data_Generate();
+            pd.Pivot_Data_Load();
+        }
+
+        #region MENI BIBLIOTEKA
+        private void dodajBibliotekuStrana_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Biblioteke/DodavanjeBiblioteke.xaml", UriKind.Relative);
+        }
+
+        private void pregledSvihBiblioteki_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Biblioteke/PregledSvihBiblioteka.xaml", UriKind.Relative);
+        }
+
+        private void brisanjeBiblioteke_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Biblioteke/BrisanjeBiblioteke.xaml", UriKind.Relative);
+        }
+
+        private void izmenaBiblioteke_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Biblioteke/IzmenaBiblioteke.xaml", UriKind.Relative);
+        }
+		
+        private void pregledKorinsikaPoBiblioteci_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Biblioteke/SpisakBibliotekaPoKorisniku.xaml", UriKind.Relative);
+        }
+        #endregion
+
+        #region MENI KNJIGE
+        private void dodajKnjigu_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Knjige/DodajKnjigu.xaml", UriKind.Relative);
+        }
+
+        private void izmeniKnjigu_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Knjige/IzmeniKnjigu.xaml", UriKind.Relative);
+        }
+
+        private void brisanjeKnjige_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Knjige/ObrisiKnjigu.xaml", UriKind.Relative);
+        }
+
+        private void pregledSvihKnjiga_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Knjige/PrikazSvihKnjiga.xaml", UriKind.Relative);
+        }
+        #endregion
+
+        #region MENI KORISNIK
+        private void dodajKorisnika_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Korisnici/DodajKorisnika.xaml", UriKind.Relative);
+        }
+
+        private void izmeniKorisnika_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Korisnici/IzmeniKorisnika.xaml", UriKind.Relative);
+        }
+
+        private void brisanjeKorisnika_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Korisnici/BrisanjeKorisnika.xaml", UriKind.Relative);
+        }
+
+        private void pregledSvihKorinsika_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Korisnici/PrikazSvihKorisnika.xaml", UriKind.Relative);
+        }
+
+        private void uclaniKorisnika_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Korisnici/UclaniKorisnikaUBiblioteku.xaml", UriKind.Relative);
+        }
+
+        private void pozajmiKnjigu_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Korisnici/PozajmiKnjigu.xaml", UriKind.Relative);
+        }
+
+        private void dodajKnjiguUBiblioteku_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("/Knjige/DodajKnjiguUBiblioteku.xaml", UriKind.Relative);
+        }
+        #endregion
+
+        #region O PROGRAMU
+        private void oprogramu_Click(object sender, RoutedEventArgs e)
+        {
+            aktivnost.Source = new Uri("Program/OProgramu.xaml", UriKind.Relative);
+        }
+        #endregion
+    }
+}
